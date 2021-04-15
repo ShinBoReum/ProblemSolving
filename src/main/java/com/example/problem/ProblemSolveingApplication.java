667b73b3946fd0115e4e5292cedb39d0acd6847b
ShinBoReum <tlsqhfma@naver.com>
2021-04-15 22:41:53 +0900

    simple Array Sum

---
+++ src/main/java/com/example/problem/ProblemSolveingApplication.java
@@ -0,0 +1,13 @@
+package com.example.problem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProblemSolveingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProblemSolveingApplication.class, args);
+    }
+
+}
